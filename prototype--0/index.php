32f21ff012014c8ba61194b6fa23e0aca96472bb
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-02-04 20:43:08 +0100

    add

--- prototype--0/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$fichier = file_get_contents("person.json");
-$data = json_decode($fichier);
-?>
-<a href="insert.php">insert</a>
--- prototype--0/inser.php
+++ prototype--0/inser.php
@@ -7,11 +7,13 @@ if(!empty($_POST)){
     $firstName = $_POST["FirstName"];
     $LastName = $_POST["LastName"];
     $Age = $_POST["Age"];
-$person=array($LastName,$Age,$FirstName);
+$person= array($LastName,$Age,$firstName);
 
     $fichier = file_get_contents("person.json");
-    $data=json_decode($data,true);
+    $data=json_decode($fichier,true);
     array_push($data,$person);
+    file_put_contents("person.json",json_encode($data));
+    header("Location: indx.php");
 }
 
 
@@ -19,7 +21,8 @@ $person=array($LastName,$Age,$FirstName);
 ?>
 
 <h1>insert your information</h1>
-<form action=""method='post'></form>
+<form method='POST'>
+    
 <label for="First Name">First Name</label>
 <input type="text"name="FirstName" >
 <br>
@@ -30,3 +33,4 @@ $person=array($LastName,$Age,$FirstName);
 <input type="text"name="Age" >
 <br>
 <button type="submit">ajoute</button>
+</form>
